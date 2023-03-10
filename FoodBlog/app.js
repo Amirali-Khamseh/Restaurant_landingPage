a05f564f45cc8afb5dead2ead45fa6534cc8a828
Amir <72108341+Amirali-Khamseh@users.noreply.github.com>
2023-03-10 22:49:14 +0100

    Leaftlet map with the marker added to the footer

--- FoodBlog/app.js
+++ FoodBlog/app.js
@@ -1,3 +1,5 @@
+
+//Seeting up Icons on the First section
 const icons  =  document.querySelectorAll('.section1-icons i');
 let i = 1;
 setInterval(()=>{
@@ -13,4 +15,24 @@ setInterval(()=>{
         }
 
 },1200);
-console.log(icons);
+
+
+//Bringing map on the footer
+const map = L.map('map').setView([0, 0], 13);
+
+	const tiles = L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+		maxZoom: 19,
+		attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+	}).addTo(map);
+
+	const marker = L.marker([51.5, -0.09]).addTo(map)
+		.bindPopup('<b>This is your Restaurant</b>').openPopup();
+
+  function onMapClick(e) {
+		popup
+			.setLatLng(e.latlng)
+			.setContent(`You clicked the map at ${e.latlng.toString()}`)
+			.openOn(map);
+	}
+
+	map.on('click', onMapClick);
