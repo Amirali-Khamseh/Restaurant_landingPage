5db554e4a29cb000d8a3b3cd694c19a91f4d63e2
Amirali Shaban Khamseh <amsha@ad.tu-chemnitz.de>
2023-03-10 18:21:55 +0100

    Three sections of the webpage done including the slider of icons , menu section and popular items

---
+++ FoodBlog/app.js
@@ -0,0 +1,16 @@
+const icons  =  document.querySelectorAll('.section1-icons i');
+let i = 1;
+setInterval(()=>{
+    i++;
+    const icon = document.querySelector('.change');
+    icon.classList.remove('change');
+        if(i>icons.length){
+            icons[0].classList.add('change');
+            i = 1;
+            
+        }else{
+            icon.nextElementSibling.classList.add('change')
+        }
+
+},1200);
+console.log(icons);
